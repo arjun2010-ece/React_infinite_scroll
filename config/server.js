bbcb42e90ee2554ea5b7248f7cba8e5a397a5de4
arjun2010.ece <arjun2010.ece@gmail.com>
2019-08-18 19:42:41 +0100

    infinite scrolling working perfectly

--- config/server.js
+++ config/server.js
@@ -1,5 +1,5 @@
 module.exports = {
-  APPLICATION_ID: process.env.APPLICATION_ID || 'REPLACE',
-  SECRET: process.env.SECRET || 'REPLACE',
+  APPLICATION_ID: process.env.APPLICATION_ID || '78cc171fed938effa77d700448187064e8ec4e65ac3b78ecab3d77323a16e3ed',
+  SECRET: process.env.SECRET || 'cd2eb14aa5dc354490b899d0315a97b0554d3c1802aa5f2dfe1787cd67f05235',
   CALLBACK_URL: process.env.CALLBACK_URL || 'http://localhost:3000'
 };
